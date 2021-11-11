3aa228aa371e31be123a18b0e916d523a93b36b8
Joey-JJ <58442698+Joey-JJ@users.noreply.github.com>
2021-11-11 22:54:43 +0100

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,104 +1,39 @@
-# Creating the todo list
-todos = []
+import functions as fn
 
-# Functionality
-def reset_id():
-    '''Reassigning the index to todos'''
-    for todo in range(len(todos)):
-        todos[todo]['id'] = todo + 1
-
-def add_todo():
-    '''Function to add todos to the todo list'''
-    todo_name = input('Enter todo name: ').title()
-    todo_task = input('Enter the todo: ').title()
-
-    # Creating a dictionary for the todo item
-    todo_dict = {'name': todo_name, 'task': todo_task, 'completed': False}
-
-    # Checking if input is valid, if so, appending the item 
-    if len(todo_name) < 1:
-        print('Invalid input')
-    else:
-        todos.append(todo_dict)
-    reset_id()
-
-def remove_todo():
-    '''Function to remove todos from the todo list'''
-    name_remove = input('Enter the name of the todo you want to remove: ').title()
-    # redefining list without the todo which needs to be removed
-    todos[:] = [todo for todo in todos if todo.get('name') != name_remove]
-    reset_id()
-
-def list_todos():
-    '''Function to display all todos to the user'''
-    if len(todos) > 0:
-        print(f'\n{len(todos)} todo(s) left\n')
-        for todo in todos:
-            name_str = todo['name']
-            task_str = todo['task']
-            completed_str = ''
-            # Making a string to show cross when a todo has been completed
-            if todo['completed']:
-                completed_str = '[x]'
-            else:
-                completed_str = '[ ]'
-            print(f"ID: {todo['id']}\nName: {name_str}\nTask: {task_str}\nCompleted: {completed_str}\n")
-    else:
-        print('Todo list empty')
-
-def mark_completed():
-    '''Function to mark a todo as "completed"'''
-    name_todo = input('Enter the name of completed todo: ').title()
-    for i in todos:
-        if i['name'] == name_todo:
-            i['completed'] = True
-
-def mark_uncompleted():
-    '''Function to mark a todo as "uncompleted"'''
-    name_todo = input('Enter the name of uncompleted todo: ').title()
-    for i in todos:
-        if i['name'] == name_todo:
-            i['completed'] = False
-
-def remove_completed():
-    '''Function to remove all completed todos from the list'''
-    todos[:] = [todo for todo in todos if todo.get('completed') != True]
-    reset_id()
 
 def init():
-    '''Logic of the program'''
-    # Welcome message
-    print('-------- Welcome to To-do --------')
-    print('Type \'help\' for a list of commands')
-
-    # Program logic
+    fn.welcome_msg()
     running = True
+    todos = []
+    commands = '\n--- Commands ---\n\
+                add - add a todo to the list\n\
+                remove - remove a todo from the list\n\
+                list - get an overview of your todos\n\
+                complete - mark a todo as completed\n\
+                uncomplete - mark a todo as uncompleted\n\
+                remove completed - remove all completed todos\n\
+                quit - Quit the program\n'
     while running:
         user_input = input('What would you like to do? ').lower()
         if user_input == 'help':
-            print('\n--- Commands ---\nadd - add a todo to the list\nremove - remove a todo from the list\nlist - get an overview of your todos\ncomplete - mark a todo as completed\nuncomplete - mark a todo as uncompleted\nremove completed - remove all completed todos\nquit - Quit the program\n')
+            print(commands)
         elif user_input == 'add':
-            add_todo()
-            print('Todo added')
+            fn.add_todo(todos)
         elif user_input == 'remove':
-            remove_todo()
-            print('Todo removed')
+            fn.remove_todo(todos)
         elif user_input == 'list':
-            list_todos()
+            fn.list_todos(todos)
         elif user_input == 'quit':
             running = False
-            print('Program ended')
         elif user_input == 'complete':
-            mark_completed()
-            print('Done')
+            fn.mark_completed(todos)
         elif user_input == 'uncomplete':
-            mark_uncompleted()
-            print('Done')
+            fn.mark_uncompleted(todos)
         elif user_input == 'remove completed':
-            remove_completed()
-            print('Deleted completed todos')
+            fn.remove_completed(todos)
         else:
             print('Invalid input. Type \'help\' for a list of commands')
 
 
-init()
+if __name__ == '__main__':
+    init()
